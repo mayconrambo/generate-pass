2647d2ed80bfac4c1ee10968e8bf844ce3db23a7
Maycon Willian Rambo <maycon.rambo11@gmail.com>
2025-04-18 20:17:47 -0300

    Adicionando google adsense no site.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
@@ -28,6 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2647387596728960"
+          crossOrigin="anonymous"
+        />
         {children}
         <Toaster />
       </body>
