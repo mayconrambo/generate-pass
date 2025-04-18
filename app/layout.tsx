75647070dd1350f5944e081d631e4f5dbeb3cb9f
Maycon Willian Rambo <maycon.rambo11@gmail.com>
2025-04-18 20:30:12 -0300

    ajuste google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
@@ -26,14 +25,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Script
+      <head>
+        {/* Script do Google Adsense */}
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2647387596728960"
           crossOrigin="anonymous"
-        />
+        ></script>
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
         <Toaster />
       </body>
